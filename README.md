d60d477c9000faece2e05f120386faf225cc40cd
ytang5 <ytang5@trip.com>
2021-05-28 15:11:44 +0800

    feat: 新增案例代码

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 - [puppeteer简介](https://github.com/twosugar/puppeteer-notes/blob/master/notes/introduction.md)
 
 - [安装](https://github.com/twosugar/puppeteer-notes/blob/master/notes/install.md)
+
+- [一个简单的demo](https://github.com/twosugar/puppeteer-notes/blob/master/notes/first-demo.md)
---
+++ demo/first-demo.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        slowMo: 10,    //放慢速度
+        headless: false,
+        defaultViewport: { width: 1440, height: 780 },
+        ignoreHTTPSErrors: false, //忽略 https 报错
+        args: ['--start-fullscreen'] //全屏打开页面
+    });
+    const pages = await browser.pages()
+    let page
+    if (pages.length === 1) {
+        page = pages[0]
+    } else {
+        page = await browser.newPage();
+    }
+    await page.goto('https://www.baidu.com');
+    const inputElement = await page.$('#kw'); //获取百度输入框dom
+    await inputElement.type('puppeteer.js中文文档', { delay: 20 }); //输入框填值，delay模拟用户输入时间 
+    const buttonElement = await page.$('.bg.s_btn') //“百度一下按钮”
+    await buttonElement.click() //点击按钮
+    await page.waitForSelector('.result.c-container.new-pmd a') //等待第一个元素出现
+    const resElement = await page.$('.result.c-container.new-pmd a')
+    await resElement.click() ////点击跳转
+})();
